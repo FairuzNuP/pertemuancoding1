85b4b37216014694d734dca5c91f72c6b9d9bdf0
FairuzNuP <126585174+FairuzNuP@users.noreply.github.com>
2025-09-20 16:47:57 +0700

    Create Three.js

---
+++ Three.js
@@ -0,0 +1,2 @@
+<script src="https://cdn.jsdelivr.net/npm/three@0.150.1/build/three.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/three@0.150.1/examples/js/controls/OrbitControls.js"></script>
