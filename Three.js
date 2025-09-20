184db7a93947251487d129c0b11b66bea8011528
FairuzNuP <126585174+FairuzNuP@users.noreply.github.com>
2025-09-20 16:51:09 +0700

    Delete Three.js

--- Three.js
+++
@@ -1,2 +0,0 @@
-<script src="https://cdn.jsdelivr.net/npm/three@0.150.1/build/three.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/three@0.150.1/examples/js/controls/OrbitControls.js"></script>
